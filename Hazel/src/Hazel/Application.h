866f43663bfddf92fa7b053d77535e61701f0a99
tobisawa <mozichanglu6@gmail.com>
2025-08-05 20:17:30 +0800

    first commit

---
+++ Hazel/src/Hazel/Application.h
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include "Core.h"
+
+namespace Hazel {
+
+	class HAZEL_API Application
+	{
+	public:
+		 Application();
+		 virtual ~Application();
+
+		void Run();
+	};
+
+	//´´½¨app
+	Application* CreateApplication();
+
+
+
+
+}
+
+
+
